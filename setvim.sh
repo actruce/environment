2648d932e2d6428c4718ceb9a43d01e58b788ad3
actruce <actruce@gmail.com>
2020-11-13 14:53:43 +0900

    Update setvim.sh

--- setvim.sh
+++ setvim.sh
@@ -1,2 +1,2 @@
-mkdir .vim                                                                                                                        |~
+mkdir .vim
 curl -LO ~/.vimrc https://github.com/actruce/environment/blob/main/.vimrc
