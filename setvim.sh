7e534b6c7e1daf2f250896d3b81cfdf482641a89
actruce <actruce@gmail.com>
2020-11-13 15:00:22 +0900

    Update setvim.sh

--- setvim.sh
+++ setvim.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 mkdir .vim
 curl -O https://raw.githubusercontent.com/actruce/environment/main/.vimrc
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+apt install exuberant-ctags
